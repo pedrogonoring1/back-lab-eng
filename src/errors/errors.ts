a92f5e0d402a1e70924fd34b8133bb87b3e859d9
Samuel <samuel.souto7@gmail.com>
2022-09-11 17:33:44 -0300

    refactoring errors

--- src/errors/errors.ts
+++ src/errors/errors.ts
@@ -1,26 +1,26 @@
-export const userExistsError = new Error('User already exists');
+export const userExistsError = new Error('Usuário já existe');
 userExistsError.name = 'UserExists';
 
-export const dogExistsError = new Error('Dog already exists');
+export const dogExistsError = new Error('Cachorro já existe');
 dogExistsError.name = 'DogExists';
 
-export const adoptionExistsError = new Error('Adoption already exists');
+export const adoptionExistsError = new Error('Adoção já existe');
 adoptionExistsError.name = 'AdoptionExists';
 
-export const userNotFoundError = new Error('User not found');
+export const userNotFoundError = new Error('Usuário não foi encontrado');
 userNotFoundError.name = 'UserNotFound';
 
-export const incorrectPasswordError = new Error('Incorrect Password');
+export const incorrectPasswordError = new Error('Senha incorreta');
 incorrectPasswordError.name = 'IncorrectPassword';
 
-export const userNotVerifiedError = new Error('User Not Verified Yet');
+export const userNotVerifiedError = new Error('Usuário não foi verificado');
 userNotVerifiedError.name = 'UserNotVerified';
 
-export const sendingEmailError = new Error('Sending Email Error');
+export const sendingEmailError = new Error('Erro ao enviar email');
 sendingEmailError.name = 'SendingEmail';
 
-export const invalidCpfOrCnpjError = new Error('Invalid CPF or CNPJ');
+export const invalidCpfOrCnpjError = new Error('CPF ou CNPJ inválido');
 invalidCpfOrCnpjError.name = 'InvalidCpfOrCnpj';
 
-export const invalidEmailError = new Error('Invalid email');
+export const invalidEmailError = new Error('Email inválido');
 invalidEmailError.name = 'InvalidEmail';
